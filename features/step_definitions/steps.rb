5998b0e1e937c371ecd83b6b950ab97ae8a7587c
dschwertfeger <david.schwertfeger@googlemail.com>
2012-11-05 10:59:28 +0100

    Added title functionalty for show page

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -31,3 +31,12 @@ Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
     assert page.has_xpath?('//*', :text => regexp)
   end
 end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
